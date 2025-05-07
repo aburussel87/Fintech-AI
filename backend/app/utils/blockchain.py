e3d53704efab83ca15c3ceac819bbb5a0cf8fcae
provat71 <159512350+provat71@users.noreply.github.com>
2025-05-07 15:43:18 +0600

    Delete backend directory

--- backend/app/utils/blockchain.py
+++
@@ -1,4 +0,0 @@
-def log_to_blockchain(tx_data: dict):
-    # 1. Convert tx_data into hash
-    # 2. Push hash to blockchain
-    pass
--- backend/app/utils/db.py
+++
@@ -1,7 +0,0 @@
-import cx_Oracle
-from app.config import DB_USERNAME, DB_PASSWORD, DB_DSN
-
-def get_connection():
-    # 1. Connect to DB using cx_Oracle
-    # 2. Return connection object
-    pass
